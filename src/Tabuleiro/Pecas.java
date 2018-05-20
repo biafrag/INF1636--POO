3133dd8c3a6ccb20c9a814b5b68a0f4904f0ccd2
Bianca <33526317+biafrag@users.noreply.github.com>
2018-05-20 00:10:22 -0300

    Colocando caminho mais generalizado de imagens

--- src/Tabuleiro/Pecas.java
+++ src/Tabuleiro/Pecas.java
@@ -4,7 +4,7 @@ import java.awt.Image;
 
 public abstract class Pecas  {	
 	//TEM COMO FAZER SEM PRECISAR DO CAMINHO?
-	static String url = "C:\\Users\\bianc\\OneDrive\\Documentos\\GitHub\\INF1636--POO\\src\\Pecas\\";
+	static String url = "src/Pecas/";
 		
 	static Image a;
 
