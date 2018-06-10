56239dde810afb8fc82faf68be9b96e997d36a2f
Bianca <33526317+biafrag@users.noreply.github.com>
2018-06-10 03:06:51 -0300

    commit

--- src/Interaction/Mouse.java
+++ src/Interaction/Mouse.java
@@ -48,53 +48,61 @@ public class Mouse extends Observable implements MouseListener {
 			t=Tabuleiro.getTabuleiro();
 			firsttime=false;
 		}
-		System.out.println("Posicao X "+e.getX());
-		System.out.println("Posicao Y "+e.getY());
-		if(mousepress==false)
+		if(e.getButton()==MouseEvent.BUTTON1)
 		{
-			System.out.println("click1");
-			x1=e.getX()+40;
-			y1=e.getY()+40;
-			if (Tabuleiro.TemPeca(x1, y1) == false) 
+			System.out.println("Posicao X "+e.getX());
+			System.out.println("Posicao Y "+e.getY());
+			if(mousepress==false)
 			{
-				System.out.println("nao tem peca no 1 click");
-				return;
+				System.out.println("click1");
+				x1=e.getX()+40;
+				y1=e.getY()+40;
+				if (Tabuleiro.TemPeca(x1, y1) == false) 
+				{
+					System.out.println("nao tem peca no 1 click");
+					return;
+				}
+				else
+				{
+					Tabuleiro.Acende(x1, y1);	
+					t.CatchPossibleMoves(x1, y1);
+					t.CatchPossibleEats(x1, y1);
+					this.setChanged();	
+					notifyObservers();
+				}
+				mousepress=true;
 			}
 			else
 			{
-				Tabuleiro.Acende(x1, y1);	
-				t.CatchPossibleMoves(x1, y1);
-				t.CatchPossibleEats(x1, y1);
-				this.setChanged();	
-				notifyObservers();
+				System.out.println("click2");
+				x2=e.getX()+40;
+				y2=e.getY()+40;
+				mousepress=false;
+				if (Tabuleiro.TemPeca(x2, y2) == false)
+				{
+					t.MexePeca(x1,y1,x2,y2);
+					this.setChanged();
+					notifyObservers();
+				}	
+				else 
+				{
+					t.ComePeca(x1,y1,x2,y2);
+					this.setChanged();
+					notifyObservers();
+				}						
 			}
-			mousepress=true;
-		}
-		else
-		{
-			System.out.println("click2");
-			x2=e.getX()+40;
-			y2=e.getY()+40;
-			mousepress=false;
-			if (Tabuleiro.TemPeca(x2, y2) == false)
+			if(t.getPeaoChange()==true)
 			{
-				t.MexePeca(x1,y1,x2,y2);
-				this.setChanged();
-				notifyObservers();
-			}	
-			else 
-			{
-				t.ComePeca(x1,y1,x2,y2);
-				this.setChanged();
-				notifyObservers();
-			}						
+				JPopupMenu popup=t.CriaPopup();
+				t.setPositionPeao(x2,y2);
+				popup.show(e.getComponent(),e.getX(), e.getY()); 	
+				t.setPeaoChange(false);
+			}
 		}
-		if(t.getPeaoChange()==true)
+		if(e.getButton()==MouseEvent.BUTTON3)
 		{
-			JPopupMenu popup=t.CriaPopup();
-			t.setPositionPeao(x2,y2);
-			popup.show(e.getComponent(),e.getX(), e.getY()); 	
-			t.setPeaoChange(false);
+			JPopupMenu popupsave=t.CriaPopupSave();
+			popupsave.show(e.getComponent(),e.getX(), e.getY()); 
 		}
 		
 	}
